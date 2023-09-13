cec45063b5ac8e1bd8b9f4a29737972f4d28318f
Łukasz Grzesiak <grzluk22@gmail.com>
2023-09-13 13:39:35 +0200

    References

---
+++ src/Controller/ReferenceController.php
@@ -0,0 +1,133 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Reference;
+use App\Repository\ReferenceRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use OpenApi\Annotations as OA;
+
+class ReferenceController extends AbstractController
+{
+    /**
+     * Pobiera numery referencyjne
+     *
+     * @OA\Tag(name="Reference")
+     * @OA\RequestBody(
+     *     request="ReferenceGetBody",
+     *     description="Parametry reference",
+     *     required=true,
+     *     @OA\JsonContent(
+     *                     example={
+     *                         "id_article": 26,
+     *                         "type": 2,
+     *                     }
+     *    )
+     * )
+     * @OA\Response(
+     *     response=200,
+     *     description="Lista numerów referencyjnych",
+     *     content={
+     *             @OA\MediaType(
+     *                 mediaType="application/json",
+     *                     example={
+     *                      "id": 1,
+     *                      "id_article": 26,
+     *                      "type": 2,
+     *                      "brand": "BREMBO",
+     *                      "number": "B156O1"
+     *                     }
+     *             )
+     *         })
+     * )
+     **/
+    #[Route('/reference', name: 'app_reference_get', methods: ["GET"])]
+    public function index(ReferenceRepository $referenceRepository, Request $request)
+    {
+        /* Pobiera numery referencyjne dla id artykułu przekazanego w body żądania, jeżeli przekazano też typ to pobiera tylko te (porównawcze oraz oryginalne) */
+        $requestArray = $request->toArray();
+        if(isset($requestArray['type'])) {
+            $references = $referenceRepository->findBy(['id_article' => $requestArray['id_article'], 'type' => $requestArray['type']]);
+        }else{
+            $references = $referenceRepository->findBy(['id_article' => $requestArray['id_article']]);
+        }
+        return $this->json($references);
+
+    }
+    /**
+     * Wstawia numery referencyjne
+     *
+     * @OA\Tag(name="Reference")
+     * @OA\RequestBody(
+     *     request="ReferencePostBody",
+     *     description="Parametry reference",
+     *     required=true,
+     *     @OA\JsonContent(
+     *                     example={
+     *                      "id_article": 26,
+     *                      "type": 2,
+     *                      "brand": "BREMBO",
+     *                      "number": "B156O1"
+     *                     }
+     *    )
+     * )
+     * @OA\Response(
+     *     response=200,
+     *     description="Lista numerów referencyjnych",
+     *     content={
+     *             @OA\MediaType(
+     *                 mediaType="application/json",
+     *                     example={
+     *                      "id": 1,
+     *                      "id_article": 26,
+     *                      "type": 2,
+     *                      "brand": "BREMBO",
+     *                      "number": "B156O1"
+     *                     }
+     *             )
+     *         })
+     * )
+     **/
+    #[Route('/reference', name: 'app_reference_post', methods: ["POST"])]
+    public function post(ReferenceRepository $referenceRepository, Request $request)
+    {
+        /* Wstawia nowy numer referencyjny dla danego artykułu */
+        $requestArray = $request->toArray();
+        $reference = new Reference();
+        $reference->setIdArticle($requestArray['id_article']);
+        $reference->setBrand($requestArray['brand']);
+        $reference->setNumber($requestArray['number']);
+        $reference->setType($requestArray['type']);
+        $referenceRepository->save($reference, true);;
+        return $this->json($reference);
+    }
+    /**
+     * Usuwa numery referencyjne
+     *
+     * @OA\Tag(name="Reference")
+     * @OA\RequestBody(
+     *     request="ReferenceDeleteBody",
+     *     description="Parametry reference",
+     *     required=true,
+     *     @OA\JsonContent(
+     *                     example={
+     *                         "id": 1
+     *                     }
+     *    )
+     * )
+     * @OA\Response(
+     *     response=200,
+     *     description="Usunieto",
+     * )
+     **/
+    #[Route('/reference', name: 'app_reference_delete', methods: ["DELETE"])]
+    public function delete(ReferenceRepository $referenceRepository, Request $request)
+    {
+        /* Usuwa numer referencyjny */
+        $requestArray = $request->toArray();
+        $reference = $referenceRepository->findOneBy(['id' => $requestArray['id']]);
+        $referenceRepository->remove($reference, true);
+    }
+}
---
+++ src/Entity/Reference.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ReferenceRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ReferenceRepository::class)]
+class Reference
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    public ?int $id = null;
+
+    #[ORM\Column]
+    public ?int $id_article = null;
+
+    #[ORM\Column]
+    public ?int $type = null;
+
+    #[ORM\Column(length: 255)]
+    public ?string $brand = null;
+
+    #[ORM\Column(length: 255)]
+    public ?string $number = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getIdArticle(): ?int
+    {
+        return $this->id_article;
+    }
+
+    public function setIdArticle(int $id_article): static
+    {
+        $this->id_article = $id_article;
+
+        return $this;
+    }
+
+    public function getType(): ?int
+    {
+        return $this->type;
+    }
+
+    public function setType(int $type): static
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    public function getBrand(): ?string
+    {
+        return $this->brand;
+    }
+
+    public function setBrand(string $brand): static
+    {
+        $this->brand = $brand;
+
+        return $this;
+    }
+
+    public function getNumber(): ?string
+    {
+        return $this->number;
+    }
+
+    public function setNumber(string $number): static
+    {
+        $this->number = $number;
+
+        return $this;
+    }
+}
