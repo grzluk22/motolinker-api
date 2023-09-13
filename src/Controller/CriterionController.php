c1fb27bad88e137326806e4a18116ee668d474c4
Łukasz Grzesiak <grzluk22@gmail.com>
2023-09-13 11:16:54 +0200

    porawki w CriterionController

--- src/Controller/CriterionController.php
+++ src/Controller/CriterionController.php
@@ -20,6 +20,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use OpenApi\Annotations as OA;
+use function Symfony\Component\Cache\Traits\object;
 
 class CriterionController extends AbstractController
 {
@@ -33,33 +34,16 @@ class CriterionController extends AbstractController
      *     content={
      *             @OA\MediaType(
      *                 mediaType="application/json",
-     *                 @OA\Schema(
-     *                     @OA\Property(
-     *                         property="id",
-     *                         type="int",
-     *                         description="Unikalne ID"
-     *                     ),
-     *                     @OA\Property(
-     *                         property="translations",
-     *                         type="array",
-     *                         description="Tablica tłumaczeń",
-     *                      @OA\Items(
-     *                          @OA\Property(
-     *                              property="pl",
-     *                              type="string",
-     *                              description="tłumaczenie_pl"
-     *                          ))
-     *                      ),
      *                     example={
      *                      "id": 1,
      *                     "translations":{
-     *                              {"id_criterion":1,
-     *                                   {"pl":"Strona Mocowania"}
-     *                                   }
-     *     }
+     *                          "id": 3,
+     *                          "id_criterion": 1,
+     *                          "id_language": 6,
+     *                          "name": "Strona mocowania"
+     *                      }
      *
      *                     }
-     *                 )
      *             )
      *         })
      * )
@@ -85,66 +69,40 @@ class CriterionController extends AbstractController
     #[Route('/criterion/{id}', name: 'app_criterion_get_one', methods: ['GET'])]
 
     /**
-     * Dodaje nowe kryteria
+     * Tworzy nowe kryterium
      *
      * @OA\Tag(name="Criterion")
      * @OA\RequestBody(
-     *     request="CriterionRequestBody",
+     *     request="CriterionAddRequestBody",
      *     description="Kryterium",
      *     required=true,
      *     @OA\JsonContent(
-     *        allOf={
-     *                 @OA\Schema(
-     *                     @OA\Property(
-     *                         property="translations",
-     *                         type="array",
-     *                         description="Tablica tłumaczeń",
-     *                      @OA\Items(
-     *                          @OA\Property(
-     *                              property="pl",
-     *                              type="string",
-     *                              description="tłumaczenie_pl"
-     *                          ))
-     *
-     *                     ),
-     *                 )
-     *        },
      *                     example={
-     *                         "translations": {"pl": "Nowe Kryterium"}
+     *                     "translations":{
+     *                          "id_language": 6,
+     *                          "name": "Strona mocowania"
+     *                      }
+     *
      *                     }
      *    )
      * )
+
      * @OA\Response(
      *     response=200,
-     *     description="Stworzone kryterium",
+     *     description="Lista kryteriów",
      *     content={
      *             @OA\MediaType(
      *                 mediaType="application/json",
-     *                 @OA\Schema(
-     *                     @OA\Property(
-     *                         property="id",
-     *                         type="int",
-     *                         description="Unikalne ID"
-     *                     ),
-     *                     @OA\Property(
-     *                         property="translations",
-     *                         type="array",
-     *                         description="Tablica tłumaczeń",
-     *                      @OA\Items(
-     *                          @OA\Property(
-     *                              property="pl",
-     *                              type="string",
-     *                              description="tłumaczenie_pl"
-     *                          ))
-     *                      ),
      *                     example={
      *                      "id": 1,
      *                     "translations":{
-     *                                   {"pl":"Strona Mocowania"}
-     *                                  }
+     *                          "id": 3,
+     *                          "id_criterion": 1,
+     *                          "id_language": 6,
+     *                          "name": "Strona mocowania"
+     *                      }
      *
      *                     }
-     *                 )
      *             )
      *         })
      * )
@@ -155,185 +113,88 @@ class CriterionController extends AbstractController
      *
      */
     #[Route('/criterion', name: 'app_criterion_add', methods: ['POST'])]
-    public function add(ManagerRegistry $managerRegistry, CriterionRepository $criterionRepository, CriterionLanguageRepository $criterionLanguageRepository, LanguageRepository $languageRepository, Request $request) {
+    public function add(CriterionRepository $criterionRepository, CriterionLanguageRepository $criterionLanguageRepository, Request $request) {
         /* Żadanie dodania nowego kryterium przyjmuje tylko tłumaczenia nazw dla danego kryterium */
         $requestArray = $request->toArray();
-        $languageManager = $managerRegistry->getManagerForClass(Language::class);
-        $criterionManager = $managerRegistry->getManagerForClass(Criterion::class);
-        $criterionLanguageManager = $managerRegistry->getManagerForClass(CriterionLanguage::class);
-        /* Sprawdzenie czy w dla głównego (o id 1) języka istnieje już kryterium o takiej nazwie */
-        $defaultLanguage = $languageRepository->findOneById(1);
-        if(isset($requestArray['translations'][$defaultLanguage->getIsoCode()])) {
-            $searchCritertionLanguageRepository = $criterionLanguageRepository->findBy(["id_language"=>1, "name" => $requestArray['translations'][$defaultLanguage->getIsoCode()]]);
-            if(count($searchCritertionLanguageRepository) > 0) {
-                return $this->json(["error" => "Kryterium o takiej nazwie już istnieje"]);
-            }
-        }else{
-            return $this->json(["error" => "Nie przekazano tłumaczenia dla głównego języka"]);
+        $criterion = new Criterion();
+        $criterionRepository->save($criterion, true);
+        foreach ($requestArray['translations'] as $translation) {
+            $criterionLanguage = new CriterionLanguage();
+            $criterionLanguage->setIdCriterion($criterion->getId());
+            $criterionLanguage->setIdLanguage($translation['id_language']);
+            $criterionLanguage->setName($translation['name']);
+            $criterionLanguageRepository->save($criterionLanguage, true);
         }
-        /*$translatedDefaultName = array_filter();*/
-
-        /* Dodawanie tłumaczeń */
-        if (!isset($requestArray['translations'])) {
-            return $this->json(["error" => "Nie przekazano tłumaczeń"]);
-        } else {
-            if (count($requestArray['translations']) == 0) {
-                return $this->json(["error" => "Nie przekazano tłumaczeń"]);
-            } else {
-                /* Sprawdzanie czy dany język istnieje w tabeli z językami jezeli nie to dodawanie go do tej tabli */
-                foreach ($requestArray['translations'] as $languageName=>$translation) {
-                    $langResult = $languageRepository->findOneBy(["isoCode" => $languageName]);
-                    if($langResult === null) {
-                        /* Wstawianie nowego języka */
-                        $language = new Language();
-                        $language->setName($languageName);
-                        $language->setIsoCode($languageName);
-                        $languageManager->persist($language);
-                        $languageManager->flush();
-                    }
-                }
-            }
-            /* Pobieranie id wszystkich dostępnych języków */
-            $languages = $languageRepository->findAll();
-            $languageIds = [];
-            foreach ($languages as $language) {
-                $languageIds[$language->getIsoCode()] = $language->getId();
-            }
-            /* Tworzenie nowego criterion */
-            $criterion = new Criterion();
-            $criterionManager->persist($criterion);
-            $criterionManager->flush();
-            foreach ($requestArray['translations'] as $languageName=>$translation) {
-                $criterionLanguage = new CriterionLanguage();
-                $criterionLanguage->setName($translation);
-                $criterionLanguage->setIdLanguage($languageIds[$languageName]);
-                $criterionLanguage->setIdCriterion($criterion->getId());
-                $criterionManager->persist($criterionLanguage);;
-                $criterionManager->flush();
-            }
-        }
-        return $this->json($requestArray);
+        $data = (object) array_merge((array) $criterion, ["translations" => $criterionLanguageRepository->findBy(['id_criterion' => $criterion->getId()])]);
+        return $this->json($data);
     }
 
     /**
      * Edytuje kryterium
      *
-     *
-     *
      * @OA\Tag(name="Criterion")
      * @OA\RequestBody(
-     *     request="CriterionEditRequestBody",
-     *     description="Kryteria",
+     *     request="CriterionAddRequestBody",
+     *     description="Kryterium",
      *     required=true,
      *     @OA\JsonContent(
-     *        allOf={
-     *                 @OA\Schema(
-     *                      @OA\Property(
-     *                         property="id",
-     *                         type="int",
-     *                         description="ID Kryterium",
-     *                     ),
-     *                     @OA\Property(
-     *                         property="translations",
-     *                         type="array",
-     *                         description="Tablica tłumaczeń",
-     *                      @OA\Items(
-     *                          @OA\Property(
-     *                              property="pl",
-     *                              type="string",
-     *                              description="tłumaczenie_pl"
-     *                          ))
-     *
-     *                     ),
-     *                 )
-     *        },
      *                     example={
-     *                         "id": 1,
-     *                         "translations": {"pl": "Nowe Kryterium"}
+     *                      "id": 1,
+     *                     "translations":{
+     *                          "id": 3,
+     *                          "id_criterion": 1,
+     *                          "id_language": 6,
+     *                          "name": "Strona mocowania"
+     *                      }
+     *
      *                     }
      *    )
      * )
-     *
+
      * @OA\Response(
      *     response=200,
      *     description="Zaktualizowane kryterium",
      *     content={
      *             @OA\MediaType(
      *                 mediaType="application/json",
-     *                 @OA\Schema(
-     *                     @OA\Property(
-     *                         property="id",
-     *                         type="int",
-     *                         description="Unikalne ID"
-     *                     ),
-     *                     @OA\Property(
-     *                         property="translations",
-     *                         type="array",
-     *                         description="Tablica tłumaczeń",
-     *                      @OA\Items(
-     *                          @OA\Property(
-     *                              property="pl",
-     *                              type="string",
-     *                              description="tłumaczenie_pl"
-     *                          ))
-     *                      ),
      *                     example={
      *                      "id": 1,
      *                     "translations":{
-     *                              {"pl":"Strona Mocowania"}
-     *     }
+     *                          "id": 3,
+     *                          "id_criterion": 1,
+     *                          "id_language": 6,
+     *                          "name": "Strona mocowania"
+     *                      }
      *
      *                     }
-     *                 )
      *             )
      *         })
      * )
      * * @OA\Response(
      *     response=404,
-     *     description="Brak kryteriów"
+     *     description="Nie znaleziono kryterium o podanym id"
      * )
-     **/
+     *
+     */
     #[Route('/criterion', name: 'app_criterion_edit', methods: ["PUT"])]
-    public function edit(ManagerRegistry $managerRegistry, CriterionRepository $criterionRepository, CriterionLanguageRepository $criterionLanguageRepository, LanguageRepository $languageRepository, Request $request)
+    public function edit(CriterionRepository $criterionRepository, CriterionLanguageRepository $criterionLanguageRepository, Request $request)
     {
-        $criterionManager = $managerRegistry->getManagerForClass(Criterion::class);
-        $criterionLanguageManager = $managerRegistry->getManagerForClass(CriterionLanguage::class);
         $requestArray = $request->toArray();
-        if(!isset($requestArray["id"])) return $this->json(["error" => "Nie przekazano id kryterium do edycji"]);
-        if(!isset($requestArray["translations"])) return $this->json(["error" => "Nie przekazano tłumaczeń dla wybranego kryterium"]);
-        $criterion = $criterionRepository->findOneBy(["id" => 1]);
-        if($criterion === null) return $this->json(["error" => "Kryterium o podanym id nie istnieje"]);
-        /* Pobieranie id wszystkich dostępnych języków */
-        $languages = $languageRepository->findAll();
-        $languageIds = [];
-        foreach ($languages as $language) {
-            $languageIds[$language->getIsoCode()] = $language->getId();
-        }
-        $criterionLanguages = $criterionLanguageRepository->findBy(["id_criterion" => $requestArray['id']]);
-        /* Dla każdego przekazanego tłumaczenia dla danego kryterium zmieniamy i zapisujemy */
-        foreach ($requestArray["translations"] as $languageCode=>$name) {
-            /* Sprawdzanie czy język o takim isoCode istnieje w bazie danych */
-            if(!isset($languageIds[$languageCode])) {
-                return $this->json(["error" => "Język o podanym kodzie nie istnieje"]);
+        $criterion = $criterionRepository->findOneBy(['id' => $requestArray['id']]);
+        if($criterion == null) return $this->json(['error' => 'Nie znaleziono kryterium o podanym id']);
+        foreach ($requestArray['translations'] as $translation) {
+            if(!isset($translation['id'])) {
+                $criterionLanguage = new CriterionLanguage();
+                $criterionLanguage->setIdLanguage($translation['id_language']);
+                $criterionLanguage->setIdCriterion($criterion->getId());
             }else{
-                /* Sprawdzanie czy tłumaczenie dla tego kryterium w danym języku istnieje */
-                    /* aktualizacja danego tłumaczenia */
-                    $thisCriterionLanguage = $criterionLanguageRepository->findOneBy(['id_criterion' => $requestArray['id'], 'id_language' => $languageIds[$languageCode]]);
-                    if($thisCriterionLanguage == null ) {
-                        /*return $this->json(["error" => "Tłumaczenie dla tego kryterium dla tego języka (".$languageCode.") nie isnieje"]);*/
-                        /* Zamiast tego wstawianie nowego języka */
-                        $thisCriterionLanguage = new CriterionLanguage();
-                        $thisCriterionLanguage->setIdCriterion($requestArray['id']);
-                        $thisCriterionLanguage->setIdLanguage($languageIds[$languageCode]);
-                    }
-                    $thisCriterionLanguage->setName($name);
-                    $criterionLanguageRepository->save($thisCriterionLanguage);
-                    $criterionLanguageManager->persist($thisCriterionLanguage);
-                    $criterionLanguageManager->flush();
+                $criterionLanguage = $criterionLanguageRepository->findOneBy(['id' => $translation['id']]);
             }
+            $criterionLanguage->setName($translation['name']);
+            $criterionLanguageRepository->save($criterionLanguage, true);
         }
-        $updatedCriterion = $criterionLanguageRepository->findBy(['id_criterion' => $requestArray['id']]);
-        return $this->json($updatedCriterion);
+        $data = (object) array_merge((array) $criterion, ["translations" => $criterionLanguageRepository->findBy(['id_criterion' => $criterion->getId()])]);
+        return $this->json($data);
     }
 
 
