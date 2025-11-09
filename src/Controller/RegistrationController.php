9a0d33e02975f1f4ccbfa79324f4af2906f5564e
Lukasz Grzesiak <grzluk22@gmail.com>
2025-11-09 09:23:40 +0100

    Aktualizacja dokumentacji o endpointy do autoryzacji

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -9,16 +9,47 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Doctrine\Persistence\ManagerRegistry;
 use App\Entity\User;
+use OpenApi\Annotations as OA;
 
 class RegistrationController extends AbstractController
 {
+        /**
+     * Pozwala na rejestrację nowego użytkownika
+     *
+     *
+     * @OA\Tag(name="Auth")
+     * @OA\RequestBody(
+     *     request="RegisterRequestBody",
+     *     description="Login oraz hasło do rejestracji",
+     *     required=true,
+     *     @OA\JsonContent(
+     *                     example={
+     *                              "username": "admin@motolinker.local",
+     *                              "password": "superSecretPassword"
+     *                     }
+     *    )
+     * )
+     * @OA\Response(
+     *     response=200,
+     *     description="Zarejestrowano pomyślnie",
+     *     content={
+     *             @OA\MediaType(
+     *                 mediaType="application/json",
+     *                     example={
+     *                              "message": "Registered Successfully"
+     *                     }
+     *             )
+     *         })
+     * )
+     **/
+
     #[Route('/register', name: 'app_register', methods: ["POST"])]
     public function index(ManagerRegistry $doctrine, Request $request, UserPasswordHasherInterface $passwordHasher): Response
     {
 
         $em = $doctrine->getManager();
         $decoded = json_decode($request->getContent());
-        $email = $decoded->email;
+        $email = $decoded->username;
         $plaintextPassword = $decoded->password;
 
         $user = new User();
